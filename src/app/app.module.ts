2d3c653e33400c321fed786d2304f377427ca0f5
Daniel Oberbillig <danieloberbillig@gmail.com>
2021-03-26 15:27:43 +1100

    add basic auth0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,26 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// Import the module from the Auth0 SDK
+import { AuthModule } from '@auth0/auth0-angular';
+import { AuthButtonComponent } from './auth-button/auth-button.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthButtonComponent,
+    UserProfileComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
+    AuthModule.forRoot({
+      domain: 'dev-2iwfgxgj.au.auth0.com',
+      clientId: 'SLMNw8zTqWuEAQkjtEHCyJETjHW9JCpc'
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
